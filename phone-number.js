5ff90af27b2b86b17690c2b0d778c77ac1cf4676
Jeremy M. Reed <reeje76@gmail.com>
2016-08-13 20:44:52 -0600

    Stub out PhoneNumber functions.

---
+++ phone-number.js
@@ -0,0 +1,10 @@
+var NotImplementedException = require('./exceptions/NotImplementedException.js'),
+  InvalidParameterException = require('./exceptions/InvalidParameterException.js');
+
+var PhoneNumber = function(rawNumber) {
+  throw new NotImplementedException();
+};
+
+PhoneNumber.prototype.number = function() {
+  throw new NotImplementedException();
+};
