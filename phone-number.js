0c585eab72dd45e4965d04ec601a0d272eec4107
Jeremy M. Reed <reeje76@gmail.com>
2016-08-13 20:54:18 -0600

    Stub out more functions.

--- phone-number.js
+++ phone-number.js
@@ -1,11 +1,31 @@
 var NotImplementedException = require('./exceptions/NotImplementedException.js'),
   InvalidParameterException = require('./exceptions/InvalidParameterException.js');
 
+const ERROR_NUMBER = '0000000000';
+
 var PhoneNumber = function(rawNumber) {
   throw new NotImplementedException();
 };
 
 PhoneNumber.prototype.number = function() {
+  throw new NotImplementedException();
+};
+
+PhoneNumber.prototype.areaCode = function() {
+  throw new NotImplementedException();
+}
+
+/*
+ * Returns phone number in the format '(areaCode) exchange-base'
+ */
+PhoneNumber.prototype.toString = function() {
+  throw new NotImplementedException();
+}
+
+/*
+ * Remove control chars, trim, and remove 
+ */
+PhoneNumber.prototype.sanitize = function() {
   throw new NotImplementedException();
 };
 
